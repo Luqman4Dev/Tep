eb9309d635ca3d5568eae3ce962bdbef22fe2c1a
dev-source1 <habibihamoda923@gmail.com>
2025-08-06 18:24:52 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -31,7 +31,7 @@ class Config(object):
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     # set this with required zed repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/Ahmadow6/source"
+        "UPSTREAM_REPO", "https://github.com/dev-source1/Tep"
     )
     # External plugins repo
     EXTERNAL_REPO = os.environ.get("EXTERNAL_REPO", None)
